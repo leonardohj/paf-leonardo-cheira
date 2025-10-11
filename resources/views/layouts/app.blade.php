bcef08417338e60d1cbd3d32b227aa9d3746eae9
Leonardo Cheira <5481@eclisboa.net>
2025-10-12 00:20:36 +0100

    creation of the app layout and input component

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>paf</title>
+</head>
+<div id="append">
+    <x-header/>
+</div>
+<body>
+    <div class="p-4">
+        @yield('body')
+    </div>
+</body>
+</html>
